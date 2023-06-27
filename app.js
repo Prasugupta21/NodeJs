43c0d889bd2be77a1aa7e37465faa24c40198beb
Prasugupta21 <101421981+Prasugupta21@users.noreply.github.com>
2023-06-27 11:50:39 +0530

    Nodejs with expressjs

--- app.js
+++ app.js
@@ -1,36 +1,14 @@
-const http=require("http");
-const fs=require("fs");
-const url=require("url");
-const { deflate } = require("zlib");
-const myServer=http.createServer((req,res)=>{
-    if(req.url==="/favicon.ico"){
-        return res.end();
-    }
-const log=`${Date.now()}:${req.url} New req Received\n`;
-const myUrl=url.parse(req.url,true);
-console.log(myUrl);
-fs.appendFile("log.text",log,(err,data)=>{
-    switch(myUrl.pathname){
-        case"/":res.end("home page");
-        break;
-        case "/about":
-            const userName=myUrl.query.myname;
-            
-        res.end(`Hello,${userName}`);
-        break;
-        case "/search":
-            const search=myUrl.query.search_query;
-            res.end("Here are your result for "+search);
-            break;
-        default:
-        res.end("Not found");
 
-    }
-    // res.end("hello from server");
+const express=require("express");
 
+
+const app=express();
+app.get("/",(req,res)=>{
+    res.send("home page");
 });
-
+app.get("/about",(req,res)=>{
+  res.send(`hello ${req.query.name}`);
 });
-myServer.listen(8000,()=>{
+app.listen(8000,()=>{
     console.log("server started");
-})
+});
